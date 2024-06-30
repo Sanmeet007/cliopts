fad16ac8073673e897c283c5b5573f123c74eac1
Sanmeet007 <ssanmeet123@gmail.com>
2024-06-30 18:52:19 +0530

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "readme.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = "1.0.1"
+VERSION = "1.1.2"
 DESCRIPTION = "The cliopts package is a Python library for parsing command line arguments. It provides a simpler and more intuitive API with less code and easy cli argument parsing."
 
 # Setting up
